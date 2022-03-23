67d252e4a73bb20892c863458d16740125bf4557
Labham Baheti <labham7@gmail.com>
2022-03-23 12:40:18 +0530

    Photon Setup

---
+++ Assets/Scripts/ConnectToServer.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected To Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Lobby Joined");
+        SceneManager.LoadScene(1);
+    }
+}
